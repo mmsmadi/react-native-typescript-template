03f5346290ab468f9f32ed7376eaee5885e3f99b
msmadi <smadi.mohammad@outlook.com>
2022-12-11 16:59:33 +0300

    updated eslint configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,43 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  overrides: [
-    {
-      files: ['*.ts', '*.tsx'],
-      rules: {
-        '@typescript-eslint/no-shadow': ['error'],
-        'no-shadow': 'off',
-        'no-undef': 'off',
+  extends: [
+    'eslint:recommended',
+    'plugin:import/errors',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+  ],
+  plugins: ['react', 'import', 'jsx-a11y'],
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    project: './tsconfig.json',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    es6: true,
+    browser: true,
+    node: true,
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        paths: ['./src'],
       },
     },
-  ],
+    'import/ignore': ['react-native'],
+    react: {
+      version: 'detect',
+    },
+  },
+  parser: '@babel/eslint-parser',
+  rules: {
+    'linebreak-style': 0,
+    'no-prototype-builtins': 'off',
+    'import/no-unresolved': 'off',
+    'no-undef': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'no-useless-escape': 'off',
+  },
 };
